533cd48905120e5f371a596aef2c5c02ea659a8c
ghousetazeem <ghousetazeem75@gmail.com>
2023-12-24 13:18:33 +0530

    udpated the issue

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,15 +3,11 @@ import Back from "../common/back/Back"
 import "./contact.css"
 
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
       <Back title='Contact us' />
       <section className='contacts padding'>
         <div className='container shadow flexSB'>
-          <div className='left row'>
-            <iframe src={map}></iframe>
-          </div>
           <div className='right row'>
             <h1>Contact us</h1>
             <p>We're open for any suggestion or just to have a chat</p>
@@ -19,32 +15,40 @@ const Contact = () => {
             <div className='items grid2'>
               <div className='box'>
                 <h4>ADDRESS:</h4>
-                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+                <p>Muffakham Jah College of Engineering and</p>
+                <p>Technology, Hyderabad, India</p>
               </div>
               <div className='box'>
                 <h4>EMAIL:</h4>
-                <p> info@yoursite.com</p>
+                <p> lerntek@gmail.com</p>
+                <p> ghousetazeem75@gmail.com</p>
               </div>
               <div className='box'>
                 <h4>PHONE:</h4>
-                <p> + 1235 2355 98</p>
+                <p> +91-6309274038</p>
+                <p> +91-6309274038</p>
               </div>
             </div>
 
-            <form action=''>
+            <form action="https://formspree.io/f/xvoeplaa"
+              method="POST">
               <div className='flexSB'>
-                <input type='text' placeholder='Name' />
-                <input type='email' placeholder='Email' />
+                <input type='text' name="name" placeholder='Name' />
+                <input type='email' name="email" placeholder='Email' />
               </div>
-              <input type='text' placeholder='Subject' />
-              <textarea cols='30' rows='10'>
-                Create a message here...
+              <input type='text' name="subject" placeholder='Subject' />
+              <textarea cols='30' rows='10' name="message" placeholder='Create a message here...'>
               </textarea>
               <button className='primary-btn'>SEND MESSAGE</button>
             </form>
 
             <h3>Follow us here</h3>
-            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+            <div className='social'>
+              <i style={{ border: " 2px solid #6c63ff", marginRight: "8px" }} className='fab fa-facebook-f icon'></i>
+              <i style={{ border: " 2px solid #6c63ff", marginRight: "8px" }} className='fab fa-instagram icon'></i>
+              <i style={{ border: " 2px solid #6c63ff", marginRight: "8px" }} className='fab fa-twitter icon'></i>
+              <i style={{ border: " 2px solid #6c63ff", marginRight: "8px" }} className='fab fa-youtube icon'></i>
+            </div>
           </div>
         </div>
       </section>
