2abc0b142fb65a40de509efb6052e2e998a93e1d
MohammedMaaz0213 <77317846+MohammedMaaz0213@users.noreply.github.com>
2023-12-25 18:30:35 +0530

    blog page errors

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -5,7 +5,7 @@ const BlogCard = () => {
   return (
     <>
       {blog.map((val) => (
-        <div className='items shadow'>
+        <div className='blog-card items shadow'>
           <div className='img'>
             <img src={val.cover} alt='' />
           </div>
@@ -15,17 +15,17 @@ const BlogCard = () => {
                 <i className='fa fa-user'></i>
                 <label htmlFor=''>{val.type}</label>
               </span>
-              <span>
+              {/* <span>
                 <i className='fa fa-calendar-alt'></i>
                 <label htmlFor=''>{val.date}</label>
               </span>
               <span>
                 <i className='fa fa-comments'></i>
                 <label htmlFor=''>{val.com}</label>
-              </span>
+              </span> */}
             </div>
             <h1>{val.title}</h1>
-            <p>{val.desc}</p>
+          <button className="blog-btn">Read More</button>
           </div>
         </div>
       ))}
