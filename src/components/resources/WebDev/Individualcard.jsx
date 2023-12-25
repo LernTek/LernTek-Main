a2dfe5325e45545b7475721aadab318dc4560501
ghousetazeem <ghousetazeem75@gmail.com>
2023-12-25 16:36:53 +0530

    resources dataschema and card update

---
+++ src/components/resources/WebDev/Individualcard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../Individualcard.css'
+import { webdev } from './webdevdata'
+
+const Individualcard = () => {
+    return (
+        <>
+            <section className='coursesCard'>
+                <div className='container grid2'>
+                    {webdev.map((val) => (
+                        <div className='items'>
+                            <div className='content flex'>
+                                <iframe src={val.resourceLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                            <div className='price'>
+                                <h2>
+                                    {val.title}
+                                </h2>
+                            </div>
+                            <p>{val.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Individualcard
---
+++ src/components/resources/WebDev/webdevdata.js
@@ -0,0 +1,26 @@
+export const webdev = [
+    {
+        id: 1,
+        resourceLink: "https://www.youtube.com/embed/KttLrVypUSA?si=BbD485A-otVhmhFn",
+        title: "HTML",
+        description: "The superhero language of the internet! Imagine it as the blueprint telling your computer how to build the fantastic world of websites 🚀",
+    },
+    {
+        id: 2,
+        resourceLink: "https://www.youtube.com/embed/p_0naOtpnis?si=nNFq1phlRk9_4Wwd",
+        title: "CSS",
+        description: "The superhero language of the internet! Imagine it as the blueprint telling your computer how to build the fantastic world of websites 🚀",
+    },
+    {
+        id: 3,
+        resourceLink: "https://www.youtube.com/embed/KttLrVypUSA?si=BbD485A-otVhmhFn",
+        title: "JavaScript",
+        description: "The superhero language of the internet! Imagine it as the blueprint telling your computer how to build the fantastic world of websites 🚀",
+    },
+    {
+        id: 4,
+        resourceLink: "https://www.youtube.com/embed/KttLrVypUSA?si=BbD485A-otVhmhFn",
+        title: "Amaan Uddin",
+        description: "The superhero language of the internet! Imagine it as the blueprint telling your computer how to build the fantastic world of websites 🚀",
+    },
+]
